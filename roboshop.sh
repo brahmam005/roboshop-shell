a9de986a36af84446bd20cf603683e9a33bc352c
Rupesh Yerra <brahmam.mekala123@gmail.com>
2024-01-25 13:31:53 +0530

    test

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
-DOMAIN_NAME="daws76s.online"
+ZONE_ID=Z06234872TGVNTMOXNTKX # replace your zone ID
+DOMAIN_NAME="brahmam005aws.shop"
 
 for i in "${INSTANCES[@]}"
 do
